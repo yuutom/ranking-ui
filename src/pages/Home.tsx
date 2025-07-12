c60828419e4b8a137761eac2b20197b6f51db015
yuutom <yuuto19961204@gmail.com>
2025-07-12 16:21:12 +0900

    対局結果テーブル

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -39,9 +39,9 @@ export default function Home() {
     const navigate = useNavigate();
 
     return (
-        <div className="mx-auto mt-4 grid max-w-3xl grid-cols-2 gap-15 sm:px-6 lg:max-w-7xl lg:grid-flow-col-dense lg:grid-cols-2">
+        <div className="max-w-screen-md mx-auto mt-4 grid grid-cols-2 gap-5 sm:px-6 lg:grid-flow-col-dense lg:grid-cols-2">
             <section className="lg:col-span-1 lg:col-start-1 h-full flex flex-col">
-                <div className="bg-white px-4 py-2 shadow sm:rounded-4xl sm:px-6 flex-1 flex flex-col">
+                <div className="bg-white px-4 py-2 shadow sm:rounded-lg sm:px-6 flex-1 flex flex-col">
                   <div className="justify-between items-start">
                     <h2 id="timeline-title" className="mt-4 text-base font-semibold text-gray-900">
                       棋士
@@ -49,9 +49,9 @@ export default function Home() {
                     <table className="mt-4 min-w-full divide-y divide-gray-300">
                       <thead>
                       <tr>
-                          <th className="px-3 py-3.5 text-left">順位</th>
-                          <th className="px-3 py-3.5 text-left">名前</th>
-                          <th className="px-3 py-3.5 text-center">レート</th>
+                          <th className="px-1 py-3.5 text-left text-sm">順位</th>
+                          <th className="px-3 py-3.5 text-left text-sm">名前</th>
+                          <th className="px-3 py-3.5 text-center text-sm">レート</th>
                         </tr>
                       </thead>
                       <tbody className="divide-y divide-gray-200 bg-white">
@@ -82,7 +82,7 @@ export default function Home() {
             </section>
 
             <section className="lg:col-span-1 lg:col-start-2 h-full flex flex-col">
-                <div className="bg-white px-4 py-5 shadow sm:rounded-4xl sm:px-6 flex-1 flex flex-col">
+                <div className="bg-white px-4 py-5 shadow sm:rounded-lg sm:px-6 flex-1 flex flex-col">
                   <div className="justify-between items-start">
                     <h2 id="timeline-title" className="text-base font-semibold text-gray-900">
                       女流棋士
@@ -90,9 +90,9 @@ export default function Home() {
                     <table className="mt-4 min-w-full divide-y divide-gray-300">
                       <thead>
                       <tr>
-                          <th className="px-3 py-3.5 text-left">順位</th>
-                          <th className="px-3 py-3.5 text-left">名前</th>
-                          <th className="px-3 py-3.5 text-center">レート</th>
+                          <th className="px-1 py-3.5 text-left text-sm">順位</th>
+                          <th className="px-3 py-3.5 text-left text-sm">名前</th>
+                          <th className="px-3 py-3.5 text-center text-sm">レート</th>
                         </tr>
                       </thead>
                       <tbody className="divide-y divide-gray-200 bg-white">
@@ -138,40 +138,41 @@ export default function Home() {
                           >
                             <td className="px-3 py-2 text-sm text-gray-800" colSpan={5}>
                               {/* 上段：日付と対局名 */}
-                              <div className="text-sm font-medium text-gray-800 mb-1">
-                                {DateUtils.formatShortDate(row.date[0])}　{extractDisplayGameName(row.game_name)}
+                              <div className="text-sm font-medium text-gray-800 mb-2">
+                                <span className="text-xs text-gray-500">{DateUtils.formatShortDate(row.date[0])}  </span>
+                                <span className="test-md font-semibold text-gray-600"> {extractDisplayGameName(row.game_name)}</span>
                               </div>
 
                               {/* 下段：1行グリッド形式で名前・勝敗・レート */}
                               <div className="grid grid-cols-5 gap-2 text-sm text-gray-800">
                                 {/* 先手 勝敗 */}
-                                <div className="flex justify-center items-center">
+                                <div className="flex justify-end items-center">
                                   {ResultStatusIcon(row.sente_player_result)}
                                 </div>
 
                                 {/* 先手 名前＋レート */}
-                                <div className="flex flex-col justify-center items-center">
+                                <div className="flex flex-col justify-center items-center col-span-1">
                                   <div className="underline">{row.sente_player_name}</div>
                                   <div className="text-xs text-gray-500">
-                                    ({(row.sente_rating - row.sente_rating_delta).toFixed(1)} → {row.sente_rating.toFixed(1)})
+                                    ({(row.sente_rating - row.sente_rating_delta).toFixed(0)} → {row.sente_rating.toFixed(0)})
                                   </div>
                                 </div>
 
                                 {/* vs */}
-                                <div className="flex justify-center items-center text-gray-500">
+                                <div className="flex justify-center items-center text-gray-500 col-span-1">
                                   vs
                                 </div>
 
                                 {/* 後手 名前＋レート */}
-                                <div className="flex flex-col justify-center items-center">
-                                  <div>{row.gote_player_name}</div>
+                                <div className="flex flex-col justify-center items-center col-span-1">
+                                  <div className="underline">{row.gote_player_name}</div>
                                   <div className="text-xs text-gray-500">
-                                    ({(row.gote_rating - row.gote_rating_delta).toFixed(1)} → {row.gote_rating.toFixed(1)})
+                                    ({(row.gote_rating - row.gote_rating_delta).toFixed(0)} → {row.gote_rating.toFixed(0)})
                                   </div>
                                 </div>
 
                                 {/* 後手 勝敗 */}
-                                <div className="flex justify-center items-center">
+                                <div className="flex justify-start items-center col-span-1">
                                   {ResultStatusIcon(row.gote_player_result)}
                                 </div>
                               </div>
